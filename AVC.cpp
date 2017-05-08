3ccb02c4098ae346e805fcf1f731696ad59412c3
HannahPachoud <hannah.pachoud@gmail.com>
2017-05-08 14:38:49 +1200

    Add files via upload
    First attempt at getting the motors to move in their required directions proportionally.

--- AVC.cpp
+++ AVC.cpp
@@ -8,54 +8,69 @@ void quad1(){
 	}
 }
 
-void function(int leftMotor, int rightMotor){
+void turnLeft(int leftMotor, int rightMotor){
+	set_motor(0, leftMotor);
+	set_motor(1, -rightMotor);
+}
+
+void turnRight(int leftMotor, int rightMotor){
 	set_motor(0, leftMotor);
 	set_motor(1, -rightMotor);
 }
 
 int getError(){
 	take_picture();
-	//display_picture(1,0);
 	int error = 0;
 	int count = 0;
 	
 	while(count<320){
 		char pixel = get_pixel(120, count, 3);
 		
-		//printf("%d\n", pixel);
+		
 		
 		if(pixel > 100){ // if the pixel is white
 			error += count - 160; //Add its error value to the total error
 		}
-		
-		/*if(pixel>max_white){
-			max_white=pixel;
-		}
-		if(pixel>max_black && pixel<100){
-			max_black = pixel;
-		}*/
-
 
 		count = count+1;
 	}
 	return error;
 }
 
+int propResponse(int error){
+	
+	int kp = 0.5;
+	int proportional_signal;
+			
+	
+	proportional_signal = error*kp;
+	
+	return ((proportional_signal/(160*1*kp))*255);
+	}
+	
+	int der (int error, int prevError){
+		int prev_error = prevError;
+		int current_error = error;
+		int kd = 5.0;
+		int der_signal = 0;
+			
+			der_signal = (current_error-prev_error/0.1)*kd;
+			return der_signal;
+	}
+
 int main(){
 	init();
 
 	
-	int max_white = 0;
-	int max_black = 0;
-	int average = 0;
+
 	int prevError = 0;
 
 	while(true){
 		int error = getError();
 		if(error > 0){
-			turnLeft();
+			turnLeft(propResponse(error)+der(error, prevError), 50);
 		} else if (error < 0){
-			turnRight();
+			turnRight(50, propResponse(error)+der(error, prevError));
 		} else {
 
 		}
@@ -63,14 +78,7 @@ int main(){
 		if(error != 0)
 			prevError = error;
 	}
-	/*
-	average = (max_black+max_white)/2;
-	printf("%d Is your max value", max_white);	
-	printf("%d\n");
-	printf("%d Is your max black value", max_black);
-	printf("%d\n");
-	printf("%d Is your average value", average);
-	*/
+	
 
 
 }
